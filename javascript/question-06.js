6d44f864a44abcdb9fe7287ad02d34523ea19020
Mashal <Mashal.maqsood112@gmail.com>
2025-06-27 17:24:44 +0500

    Completed js question 6

---
+++ javascript/question-06.js
@@ -0,0 +1,7 @@
+let a = 108;
+let b= new Number(108);
+let c = 108;
+
+console.log(a == b) //values are compared so output is true
+console.log(a === b) //types are also compared so output is false
+console.log(b === c);  //types are also compared so output is false
