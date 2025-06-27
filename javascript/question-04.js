cd75913d4aaefa3c7f54a30c808fb2633f36f474
Mashal <Mashal.maqsood112@gmail.com>
2025-06-27 16:56:04 +0500

    Completed js question 4

---
+++ javascript/question-04.js
@@ -0,0 +1,5 @@
+console.log(+true)
+console.log(!"javascript")
+
+// + converts its operand into a number so the output is 1
+// ! inverts the boolean value so "javascript" is a truthy value so the output is false
