2abef7e3ee318b5873a501dedd8fdfadf47adbb1
Mashal <Mashal.maqsood112@gmail.com>
2025-06-28 16:05:00 +0500

    Completed js question 11

---
+++ javascript/question-11.js
@@ -0,0 +1,12 @@
+// // Three phases of event propagation
+// whenever we click on window, there are three phases
+// 1 Capturing
+// 2 Targetting
+// 3 Bubbling
+
+// the order which the event follows:
+// window
+// document
+// div
+// button 
+// targetting
