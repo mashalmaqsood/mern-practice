2a0baa5bc8fc914d25253913647aa8f64c79d96d
Mashal <Mashal.maqsood112@gmail.com>
2025-06-28 16:36:35 +0500

    Modified js question 12

--- javascript/question-12.js
+++ javascript/question-12.js
@@ -2,7 +2,7 @@ function sum(a,b){
     return a + b;
 }
 
-console.log(typeof (sum(10,"10")))
+console.log(sum(10,"10"))
 
 // If either operand is a string, JavaScript converts the other to a string and performs string concatenation.
 // 10 + "10" → JavaScript coerces the number 10 to a string "10"
