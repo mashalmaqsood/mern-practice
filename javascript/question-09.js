fa1ff322c252c1bcfd43e492937754684b2ea5f1
Mashal <Mashal.maqsood112@gmail.com>
2025-06-28 15:47:22 +0500

    Completed js question 9

---
+++ javascript/question-09.js
@@ -0,0 +1,11 @@
+function showModal(){
+    console.log(showModal.timeout)
+}
+
+showModal();
+showModal.timeout= 200;
+
+showModal.timeout = 100;
+showModal();
+
+//In js a function acts as an object
